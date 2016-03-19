8c2bb1b11d50a06ed9c8b59f397768c668dbba7e
Sam Spilsbury <smspillaz@gmail.com>
2016-03-19 08:12:14 +0800

    build: Fix typo

--- build.py
+++ build.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
         builder.add_common_builds(visual_versions=[12])
 
     if platform.system() == "Linux":
-        for ver in ["4.8", "4.9" "5.2", "5.3"]:
+        for ver in ["4.8", "4.9", "5.2", "5.3"]:
             for arch in ["x86", "x86_64"]:
                 builder.add({"arch": arch,
                              "build_type": "Release",
